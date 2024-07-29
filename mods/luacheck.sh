5498c116f535b9028c3ab92c12e9c5af9eb4ab47
Kaedenn <kaedenn@gmail.com>
2024-07-28 16:50:19 -0800

    Slight luacheck.sh improvements

--- mods/luacheck.sh
+++ mods/luacheck.sh
@@ -7,17 +7,40 @@ if [[ -e "$BASE/../utility/lua/luacheck_config.lua" ]]; then
   CONF="$BASE/../utility/lua/luacheck_config.lua"
 fi
 
-declare -a IGNORE=(131 211 212 213 311 512 542 611 612 614)
-declare -a lua_args=(--exclude-files 'ref/*')
+declare -a IGNORE=(
+  131 # Unused implicitly defined global variable.
+  211 # Unused local variable.
+  212 # Unused argument.
+  213 # Unused loop variable.
+  311 # Value assigned to a local variable is unused.
+  512 # Loop can be executed at most once.
+  542 # An empty if branch.
+  611 # A line consists of nothing but whitespace.
+  612 # A line contains trailing whitespace.
+  614 # Trailing whitespace in a comment.
+)
 
-while getopts "cIi:a:vh" opt; do
+declare -a lua_args=()
+
+while getopts "cIi:vh" opt; do
   case "$opt" in
     c) CONF="$OPTARG";;
     I) IGNORE=();;
     i) IGNORE+=("$OPTARG");;
-    a) lua_args+=("$OPTARG");;
     v) set -x;;
-    h) echo "usage: $0 [-c CONF] [-I] [-i NUM] [-a ARG] [-v] [-h]" >&2; exit 0;;
+    h) cat <<EOF >&2
+usage: $0 [-c CONF] [-I] [-i NUM] [-v] [-h] [-- args...]
+
+options:
+    -c CONF   path to config file (default $CONFIG)
+    -I        clear error ignore list (default ${IGNORE[@]})
+    -i NUM    add NUM to the error ignore list
+    -v        enable debugging
+    -h        print this message and exit
+
+All arguments after -- are passed to luacheck.
+EOF
+       exit 0;;
   esac
 done
 shift $((OPTIND-1))
@@ -26,4 +49,17 @@ if [[ ${#IGNORE[@]} -gt 0 ]]; then
   lua_args+=(--ignore ${IGNORE[@]})
 fi
 
-luacheck $@ --config "$CONF" -quiet --allow-defined --no-max-line-length --globals print_error async wait GameRegisterStatusEffect Reflection_RegisterProjectile RegisterPerk RegisterGun BIOME_NAME ${lua_args[@]}
+lua_args+=(--allow-defined)
+lua_args+=(--no-max-line-length)
+lua_args+=(--exclude-files '**/nxml.lua')
+lua_args+=(--codes)
+
+declare -a GLOBALS_EXTRA=(
+  GameRegisterStatusEffect
+  Reflection_RegisterProjectile
+  RegisterPerk
+  RegisterGun
+  BIOME_NAME
+)
+
+luacheck $@ --config "$CONF" --quiet --globals ${GLOBALS_EXTRA[@]} ${lua_args[@]}
